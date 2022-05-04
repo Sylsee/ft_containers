34adf91fc172426b6cef708313b684cf99e4c3ad
Sylsee <sylvio.poliart@gmail.com>
2022-05-04 17:50:44 +0200

    stack done

--- main.cpp
+++ main.cpp
@@ -104,7 +104,7 @@ int main(void)
 	namespace ft = std;
 #else
 	// #include <map.hpp>
-	// #include <stack.hpp>
+	#include <stack.hpp>
 	#include <vector.hpp>
 #endif
 
@@ -121,24 +121,24 @@ struct Buffer
 
 #define COUNT (MAX_RAM / (int)sizeof(Buffer))
 
-// template<typename T>
-// class MutantStack : public ft::stack<T>
-// {
-// public:
-// 	MutantStack() {}
-// 	MutantStack(const MutantStack<T>& src) { *this = src; }
-// 	MutantStack<T>& operator=(const MutantStack<T>& rhs) 
-// 	{
-// 		this->c = rhs.c;
-// 		return *this;
-// 	}
-// 	~MutantStack() {}
+template<typename T>
+class MutantStack : public ft::stack<T>
+{
+public:
+	MutantStack() {}
+	MutantStack(const MutantStack<T>& src) { *this = src; }
+	MutantStack<T>& operator=(const MutantStack<T>& rhs) 
+	{
+		this->c = rhs.c;
+		return *this;
+	}
+	~MutantStack() {}
 
-// 	typedef typename ft::stack<T>::container_type::iterator iterator;
+	typedef typename ft::stack<T>::container_type::iterator iterator;
 
-// 	iterator begin() { return this->c.begin(); }
-// 	iterator end() { return this->c.end(); }
-// };
+	iterator begin() { return this->c.begin(); }
+	iterator end() { return this->c.end(); }
+};
 
 int main(int argc, char** argv) {
 	if (argc != 2)
@@ -153,10 +153,11 @@ int main(int argc, char** argv) {
 
 	ft::vector<std::string> vector_str;
 	ft::vector<int> vector_int;
-	// ft::stack<int> stack_int;
+	ft::stack<int> stack_int;
 	ft::vector<Buffer> vector_buffer;
-	// ft::stack<Buffer, std::deque<Buffer> > stack_deq_buffer;
+	ft::stack<Buffer, std::deque<Buffer> > stack_deq_buffer;
 	// ft::map<int, int> map_int;
+
 
 	for (int i = 0; i < COUNT; i++)
 	{
@@ -200,13 +201,13 @@ int main(int argc, char** argv) {
 	// {
 		// ft::map<int, int> copy = map_int;
 	// }
-	// MutantStack<char> iterable_stack;
-	// for (char letter = 'a'; letter <= 'z'; letter++)
-		// iterable_stack.push(letter);
-	// for (MutantStack<char>::iterator it = iterable_stack.begin(); it != iterable_stack.end(); it++)
-	// {
-		// std::cout << *it;
-	// }
+	MutantStack<char> iterable_stack;
+	for (char letter = 'a'; letter <= 'z'; letter++)
+		iterable_stack.push(letter);
+	for (MutantStack<char>::iterator it = iterable_stack.begin(); it != iterable_stack.end(); it++)
+	{
+		std::cout << *it;
+	}
 	std::cout << std::endl;
 	return (0);
 }
